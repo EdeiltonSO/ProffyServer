eb1253cc1a55715b7e12cff12f914ccdd211910a
Edeilton Oliveira <edeiltonso@gmail.com>
2020-08-21 02:33:50 -0300

    new feature: class index

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,7 @@ const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
 
-// routes.get('/classes', classesController.index);
+routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
 routes.get('/connections', connectionsController.index);
